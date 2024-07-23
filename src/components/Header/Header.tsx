29163ff8aa9de985fc1e35250ab91d7316f182ca
gabicortes <gabicortes@MacBook-Air.home>
2024-07-23 07:43:27 +0200

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+type HeaderProps = {
+    title: string
+}
+
+export const Header = ({title}: HeaderProps) => {
+  return <h1>{title}</h1>;
+};
---
+++ src/components/TranscriptText/TranscriptText.tsx
@@ -0,0 +1,3 @@
+export const TranscriptText = () => {
+    return <div> TranscriptText</div>
+}
